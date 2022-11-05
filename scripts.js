3a0f6eb97e260a6cca2f2bfb651da4d6f1933a91
MNXLPZ <m.lopezc.2021@alumnos.urjc.es>
2022-11-05 19:49:22 +0100

    Actualización de scripts

--- scripts.js
+++ scripts.js
@@ -1,7 +1,35 @@
-/*!
-* Start Bootstrap - Shop Item v5.0.5 (https://startbootstrap.com/template/shop-item)
-* Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-item/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+class dish {
+    constructor(name,description,ingredients,price) {
+        this.name = name;
+        this.description = description;
+        this.ingredients = ingredients;
+        this.price = price;
+    }
+    Valoration(number)
+    if (number=1) {
+        let stars = document.getElementById('valoration')
+        stars.innerHTML= '<div class="d-flex justify-content-center small text-warning mb-2"><div class="bi-star-fill"></div></div>'
+    }
+    if (number=2) {
+        let stars = document.getElementById('valoration')
+        stars.innerHTML= '<div class="d-flex justify-content-center small text-warning mb-2"><div class="bi-star-fill"></div><div class="bi-star-fill"></div></div>'
+    }
+    if (number=3) {
+        let stars = document.getElementById('valoration')
+        stars.innerHTML= '<div class="d-flex justify-content-center small text-warning mb-2"><div class="bi-star-fill"></div><div class="bi-star-fill"></div><div class="bi-star-fill"></div></div>'
+    }
+    if (number=4) {
+        let stars = document.getElementById('valoration')
+        stars.innerHTML= '<div class="d-flex justify-content-center small text-warning mb-2"><div class="bi-star-fill"></div><div class="bi-star-fill"></div><div class="bi-star-fill"></div><div class="bi-star-fill"></div></div>'
+    }
+    if (number=5) {
+        let stars = document.getElementById('valoration')
+        stars.innerHTML= '<div class="d-flex justify-content-center small text-warning mb-2"><div class="bi-star-fill"></div><div class="bi-star-fill"></div><div class="bi-star-fill"></div><div class="bi-star-fill"></div><div class="bi-star-fill"></div></div>'
+    }  
+}
+
+
+<div class="d-flex justify-content-center small text-warning mb-2">
+        <div class="bi-star-fill"></div>
+</div>
+
