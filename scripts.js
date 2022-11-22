ea1fa3720b18b19e4502f3de21af8225a14a9e79
MNXLPZ <m.lopezc.2021@alumnos.urjc.es>
2022-11-22 15:25:11 +0100

    Actualización

--- scripts.js
+++ scripts.js
@@ -1,11 +1,16 @@
-    /*
-class dish {
-    constructor(name,description,ingredients,price) {
-        this.name = name;
-        this.description = description;
-        this.ingredients = ingredients;
-        this.price = price;
+ let productos = [
+    {
+        tipo:type,
+        nombre:name,
+        descripcion:description,
+
     }
+ ]
+ 
+ 
+ 
+ 
+ /*
 Valoration(number)
     if (number=1) {
         let stars = document.getElementById('valoration')
@@ -33,9 +38,10 @@ Valoration(number)
 //        let productFormData = new FormData(formulario);
 //        let productFormRef = document.getElementById("item_menu");
    
-   let type= document.getElementById('product_type').value;
+   let category= document.getElementById('product_category').value;
    let name= document.getElementById('product_name').value;
    let description= document.getElementById('product_description').value;
+   
    let content = document.getElementById('item_menu');
    content.innerHTML += `<div class="col mb-5">
                         <div class="card h-100">
@@ -46,7 +52,7 @@ Valoration(number)
                                 <div class="text-center">
                                     <!-- Product name-->
                                     <h5 class="fw-bolder">`+ name +`</h5>
-                                    <b>`+type+`</b>
+                                    <b>`+category+`</b> <br>
                                     <!-- Product price-->
                                     `+ description +`
                                 </div>
@@ -60,12 +66,40 @@ Valoration(number)
    `
 
 }
-function menuPrincipal(){
-    let content=document.getElementById("principal");
+function mostrarProducto(id){
+ //   let content = document.getElementById('principal');
+ //   let producto = productos [id]
+    principal.innerHTML= ` 
+        <section class="py-5">
+            <div class="container px-4 px-lg-5 my-5">
+                <div class="row gx-4 gx-lg-5 align-items-center">
+                    <div class="col-md-6"><img class="card-img-top mb-5 mb-md-0" src="https://dummyimage.com/600x700/dee2e6/6c757d.jpg" alt="..." /></div>
+                    <div class="col-md-6">
+                        <div class="small mb-1">SKU: BST-498</div>
+                        <h1 class="display-5 fw-bolder">Shop item template</h1>
+                        <div class="fs-5 mb-5">
+                            <span class="text-decoration-line-through">$45.00</span>
+                            <span>$40.00</span>
+                        </div>
+                        <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium at dolorem quidem modi. Nam sequi consequatur obcaecati excepturi alias magni, accusamus eius blanditiis delectus ipsam minima ea iste laborum vero?</p>
+                        <div class="d-flex">
+                             <input class="form-control text-center me-3" id="inputQuantity" type="num" value="1" style="max-width: 3rem" /> 
+                            <button class="btn btn-outline-dark flex-shrink-0" type="button">
+                                <span class="glyphicon glyphicon-heart"></span>
+                                Añadir a favoritos
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section> 
+    `
     
 }
+function borrar(){
+    content.innerHTML=`<p>PRUEBA</P> <button onclick="menuPrincipal()"></button>`
+}
 
-item_menu
 
 
 //item_menu
