af1e382968f33fcc2413620f0bd20d10134699ec
Adrian Esteban Martin <a.estebanm.2021@alumnos.urjc.es>
2022-11-26 14:42:37 +0100

    Mostrar producto y añadir

--- scripts.js
+++ scripts.js
@@ -1,12 +1,27 @@
- let productos = [
-    {
-        tipo:type,
-        nombre:name,
-        descripcion:description,
+function nuevoProducto(titulo, descripcion,  categoria, ingredientes,) {
 
+    let producto = {
+
+        titulo: titulo,
+        descripcion: descripcion,
+        categoria: categoria,
+        ingredientes: ingredientes,
+
+        toString: function(){
+            return 'titulo:'+this.titulo+' descripcion:'+this.descripcion+' categoria:'+this.categoria;
+        }
     }
- ]
- 
+
+    return producto;
+}
+
+let productos = [ 
+    nuevoProducto('Agua', 'Botella de agua', 'Bebida', ['agua']), 
+    nuevoProducto('Arroz', 'Arroz con tomate', 'Plato principal',['arroz', 'tomate'] ),
+    nuevoProducto('Natillas', 'Natillas de vainilla', 'Postre', ['leche', 'azucar', 'vainilla']),
+    nuevoProducto('Espaguetis', 'Espaguetis con tomate', 'Plato Principal',['espaguetis', 'tomate'] )
+];
+   
  
  
  
@@ -66,19 +81,19 @@ Valoration(number)
    `
 
 }
-function mostrarProducto(id){
- //   let content = document.getElementById('principal');
+function mostrarProducto(indice){
+   let content = document.getElementById('principal');
  //   let producto = productos [id]
-    principal.innerHTML= ` 
+ content.innerHTML= ` 
         <section class="py-5">
             <div class="container px-4 px-lg-5 my-5">
                 <div class="row gx-4 gx-lg-5 align-items-center">
                     <div class="col-md-6"><img class="card-img-top mb-5 mb-md-0" src="https://dummyimage.com/600x700/dee2e6/6c757d.jpg" alt="..." /></div>
                     <div class="col-md-6">
                         <div class="small mb-1">SKU: BST-498</div>
-                        <h1 class="display-5 fw-bolder">Shop item template</h1>
+                        <h1 class="display-5 fw-bolder">`+productos[indice].titulo+`</h1>
                         <div class="fs-5 mb-5">
-                            <span class="text-decoration-line-through">$45.00</span>
+                            <span class="text-decoration-line-through">`+productos[indice].descripcion+`</span>
                             <span>$40.00</span>
                         </div>
                         <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium at dolorem quidem modi. Nam sequi consequatur obcaecati excepturi alias magni, accusamus eius blanditiis delectus ipsam minima ea iste laborum vero?</p>
@@ -96,10 +111,61 @@ function mostrarProducto(id){
     `
     
 }
+
+function mostrarTodos(){
+    let content = document.getElementById('item_menu');
+    let indice=0;
+    for (let producto of productos) {
+        content.innerHTML += `<div class="col mb-5">
+        <div class="card h-100">
+            <!-- Product image-->
+            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+            <!-- Product details-->
+            <div class="card-body p-4">
+                <div class="text-center">
+                    <!-- Product name-->
+                    <h5 class="fw-bolder"><a onclick="mostrarProducto(`+indice+`)" href="#">`+producto.titulo+`</a></h5>
+                </div>
+            </div>
+            <!-- Product actions-->
+            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                <div class="text-center"><a class="btn btn-outline-dark mt-auto" onclick="mostrarProducto(`+indice+`)" href="#">Mas info</a></div>
+            </div>
+        </div>
+    </div> `;
+    indice=indice+1;
+    }
+}
+
+function crearElemento() {
+    let content = document.getElementById('principal');
+    content.innerHTML= `        <div class="col-xs-12 col-sm-10 col-md-8 col-lg-6 ">
+    <form id="product_form" action="" role="form">
+        <div class="form-group">
+            <label for="tipo_producto" name="tipo_producto">Categoría de producto:</label>
+            <input type="text" name="categoria_producto" class="form-control" size="25" id="product_category" placeholder="Ingresa el tipo de producto a añadir">
+        </div>
+        <div class="form-group">
+            <label for="nombre_producto" name="nombre_producto">Nombre:</label>
+            <input type="text" name="nombre_producto" class="form-control" id="product_name" placeholder="Ingresa el nombre del producto">
+        </div>
+        <div class="form-group">
+            <label for="descripción_producto" name="descripción_producto">Descripción del producto:</label>
+            <input type="text" name="descripción_producto" class="form-control" id="product_description" placeholder="Ingresa la descripción">
+        </div> <br>
+        <div class="text-center"><a class="btn btn-outline-dark mt-auto" onclick="getInfoForm()" type="submit"  href="#"><span><b>+</b></span> Añadir producto</a></div>
+        
+    </form>   
+</div>`;
+}
+
+
+
+
 function borrar(){
     content.innerHTML=`<p>PRUEBA</P> <button onclick="menuPrincipal()"></button>`
 }
 
-
+mostrarTodos();
 
 //item_menu
